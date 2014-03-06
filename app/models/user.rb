2c61761c500d282fefe4025df18b79fe08ed38a3
Joe Wilmoth <jbwilmoth@gmail.com>
2014-03-05 22:10:23 -0600

    user table built

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,6 @@ class User < ActiveRecord::Base
   validates :name, :length => { :minimum => 3, :message => "must be at least 3 characters, fool!" }
   validates :entered_password, :length => { :minimum => 6 }
   validates :email, :uniqueness => true, :format => /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  include BCrypt
 
   def password
     @password ||= Password.new(password_hash)
