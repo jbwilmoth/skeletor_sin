1d2003d1b2aeaf36db93da3fe7b2a569e29facdb
Joe Wilmoth <jbwilmoth@gmail.com>
2014-03-05 22:59:53 -0600

    final skeleton complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
 require 'faker'
 
 User.delete_all
-Event.delete_all
 
 users = 500.times.map do
   User.create :first_name => Faker::Name.first_name,
@@ -10,15 +9,4 @@ users = 500.times.map do
               :password   => "password", 
               :password_confirmation => "password",
               :birthdate  => Date.today - 15.years - rand(20000).days
-end
-
-100.times do
-  start_time = Time.now + (rand(61) - 30).days
-  end_time   = start_time + (1 + rand(6)).hours
-
-  Event.create :user_id    => users[rand(users.length)].id,
-               :name       => Faker::Company.name,
-               :location   => "#{Faker::Address.city}, #{Faker::Address.state_abbr}",
-               :starts_at  => start_time,
-               :ends_at    => end_time
 end
